d4f3dd9b976f5e7ccafe1a4e751ab517bc2b2075
Alípio Afonso de Almeida Oliveira <aolazulum@hotmail.com>
2016-05-26 21:41:14 -0300

    converts the block below to another block

---
+++ math/blockBelow.py
@@ -0,0 +1,11 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+pos = mc.player.getTilePos()
+
+x = pos.x
+y = pos.y
+z = pos.z
+
+blockType = 90
+
+mc.setBlock(x, y-1, z, blockType)
